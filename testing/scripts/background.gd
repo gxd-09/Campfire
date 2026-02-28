d8f138849d3f12d1ac88232c8070f2f2e0bf9e09
OrigamiMan <156112760+OrigamiMan@users.noreply.github.com>
2026-02-28 11:23:49 +0000

    background parallax

--- testing/scripts/background.gd
+++ testing/scripts/background.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
+const SPEED1 = 0
+const SPEED2 = -0.5
+const SPEED3 = -1
+const SPEED4 = -1.5
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,4 +12,13 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	var direction := Input.get_axis("Left", "Right")
+	if direction:
+		$"Base Layer 2".position.x += direction * SPEED2
+		$"Base Layer 2b".position.x += direction * SPEED2
+		
+		$"Base Layer 3".position.x += direction * SPEED3
+		$"Base Layer 3b".position.x += direction * SPEED3
+		
+		$"Base Layer 4".position.x += direction * SPEED4
+		$"Base Layer 4b".position.x += direction * SPEED4
